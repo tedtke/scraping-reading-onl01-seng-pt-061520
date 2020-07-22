b12252d069952d623882004066a59dfa41374195
Teodoro Garcia III <tedtke514@gmail.com>
2020-07-22 22:58:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,8 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+# html = open("https://flatironschool.com/")
 
-doc = Nokogiri::HTML(html)
+# doc = Nokogiri::HTML(html)
 
-puts doc
+# puts doc
