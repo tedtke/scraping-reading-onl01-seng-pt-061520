5f71e6111c39419bda5daa1e365551dcd84de0fc
Teodoro Garcia III <tedtke514@gmail.com>
2020-07-22 23:04:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,12 +1,7 @@
-require 'nokogiri'
-require 'open-uri'
-
-# html = open("https://flatironschool.com/")
-
-# doc = Nokogiri::HTML(html)
-
-# puts doc
-
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-
-puts doc.css(".headline-260IBN").text
+    require 'nokogiri'
+    require 'open-uri'
+     
+    html = open("https://flatironschool.com/")
+    doc = Nokogiri::HTML(html)
+     
+    doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
